ae074f5da5aa10a2354d757e3651823048ba4848
Sam Smith <samlsmith424@gmail.com>
2022-05-31 17:26:34 -0700

    Initial commit

---
+++ db/migrate/20220601002356_create_subscriptions.rb
@@ -0,0 +1,12 @@
+class CreateSubscriptions < ActiveRecord::Migration[5.2]
+  def change
+    create_table :subscriptions do |t|
+      t.references :customer, foreign_key: true
+      t.references :tea, foreign_key: true
+      t.string :title
+      t.float :price
+      t.string :status
+      t.string :frequency
+    end
+  end
+end
