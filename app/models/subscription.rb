5603be834659586d124a97fb5f576e5963bf32f0
Sam Smith <samlsmith424@gmail.com>
2022-05-31 22:15:31 -0700

    Test: validate numericality of price

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -3,6 +3,7 @@ class Subscription < ApplicationRecord
   belongs_to :tea
 
   validates_presence_of :title, :price, :status, :frequency
+  validates_numericality_of :price
   enum status: [:active, :canceled]
   enum frequency: [:weekly, :monthly, :annually]
 end
--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -10,6 +10,7 @@ RSpec.describe Subscription, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:title) }
     it { should validate_presence_of(:price) }
+    it { should validate_numericality_of(:price) }
     it { should validate_presence_of(:status) }
     it { should validate_presence_of(:frequency) }
   end
