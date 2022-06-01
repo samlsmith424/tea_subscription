010e2bb8c6107511364d58ba542e30e04499ddd2
Sam Smith <samlsmith424@gmail.com>
2022-05-31 17:33:51 -0700

    Test: model relationships

---
+++ app/models/tea.rb
@@ -0,0 +1,6 @@
+class Tea < ApplicationRecord
+  has_many :subscriptions
+  has_many :customers, through: :subscriptions
+
+  validates_presence_of :title, :description, :temperature, :brew_time
+end
