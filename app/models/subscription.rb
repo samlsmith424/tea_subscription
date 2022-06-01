5f86c937322063c760c027c395480b022cb65b5e
Sam Smith <samlsmith424@gmail.com>
2022-05-31 18:43:26 -0700

    Feat: enums for subscription status and frequency

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,8 @@
 class Subscription < ApplicationRecord
   belongs_to :customer
   belongs_to :tea
+
+  validates_presence_of :title, :price, :status, :frequency
+  enum status: [:active, :canceled]
+  enum frequency: [:weekly, :monthly, :annually]
 end
