f986edc738d3da37da5f3ad8ce9195c72133ebb0
Sam Smith <samlsmith424@gmail.com>
2022-06-01 18:31:53 -0700

    Feat: add seeds to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+customers = FactoryBot.create_list(:customer, 2)
+tea1 = FactoryBot.create(:tea)
+tea2 = FactoryBot.create(:tea)
+tea3 = FactoryBot.create(:tea)
+FactoryBot.create(:subscription, frequency: :monthly, customer_id: customers.first.id, tea_id: tea1.id)
+FactoryBot.create(:subscription, frequency: :annually, customer_id: customers.first.id, tea_id: tea2.id)
+FactoryBot.create(:subscription, frequency: :monthly, customer_id: customers.last.id, tea_id: tea3.id)
