9264b6dd6460fdb998eee155c34cebcf81b53cc9
Sam Smith <samlsmith424@gmail.com>
2022-06-02 11:08:19 -0700

    Test: json response body with proper attributes

--- spec/requests/api/v1/create_tea_spec.rb
+++ spec/requests/api/v1/create_tea_spec.rb
@@ -18,6 +18,26 @@ RSpec.describe 'create tea request' do
     expect(response).to have_http_status(201)
     expect(tea).to be_a(Hash)
     expect(tea).to have_key(:data)
+    expect(tea[:data]).to have_key(:id)
+    expect(tea[:data][:id]).to be_a(String)
+
+    expect(tea[:data]).to have_key(:type)
+    expect(tea[:data][:type]).to eq("tea")
+
+    expect(tea[:data]).to have_key(:attributes)
+    expect(tea[:data][:attributes]).to be_a(Hash)
+
+    expect(tea[:data][:attributes]).to have_key(:title)
+    expect(tea[:data][:attributes][:title]).to be_a(String)
+
+    expect(tea[:data][:attributes]).to have_key(:description)
+    expect(tea[:data][:attributes][:description]).to be_a(String)
+
+    expect(tea[:data][:attributes]).to have_key(:temperature)
+    expect(tea[:data][:attributes][:temperature]).to be_a(Integer)
+
+    expect(tea[:data][:attributes]).to have_key(:brew_time)
+    expect(tea[:data][:attributes][:brew_time]).to be_a(Integer)
   end
 
   it 'returns an error if all input values are not present' do
