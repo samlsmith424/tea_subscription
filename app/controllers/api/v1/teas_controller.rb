165cf32ab5f2406c6435df74796a5190c995b678
Sam Smith <samlsmith424@gmail.com>
2022-06-01 23:32:25 -0700

    Feat: create tea action in controller

--- app/controllers/api/v1/teas_controller.rb
+++ app/controllers/api/v1/teas_controller.rb
@@ -1,5 +1,15 @@
 class Api::V1::TeasController < ApplicationController
-  def index
+  def create
+    tea = Tea.new(tea_params)
+    if tea.save
+      render json: TeaSerializer.new(tea)
+    else
+      render json: { error: "unable to create tea" }, status: :bad_request
+    end
+  end
 
-  end 
+  private
+  def tea_params
+    params.permit(:title, :description, :temperature, :brew_time)
+  end
 end
