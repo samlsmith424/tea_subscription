1adbb5d2ebccdf070cd57cc92012992a1861f557
Sam Smith <samlsmith424@gmail.com>
2022-06-02 10:32:02 -0700

    Feat: create customer action

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -4,6 +4,17 @@ class Api::V1::CustomersController < ApplicationController
   end
 
   def create
+    customer = Customer.new(customer_params)
+    if customer.save
+      render json: CustomerSerializer.new(customer), status: 201
+    else
+      render json: { error: "unable to create customer" }, status: :bad_request
+    end
+  end
+
+  private
+  def customer_params
+    params.permit(:first_name, :last_name, :email, :address)
   end
 
 end
