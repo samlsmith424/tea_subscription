397d75bba7c86f0c3be333ad8fadee577fe51b6f
Sam Smith <samlsmith424@gmail.com>
2022-06-01 21:08:41 -0700

    Feat: valid status callback

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::SubscriptionsController < ApplicationController
+  before_action :valid_status
+
   def create
     subscription = Subscription.new(subscription_params)
     if subscription.save
@@ -10,7 +12,7 @@ class Api::V1::SubscriptionsController < ApplicationController
 
   def update
     subscription = Subscription.find(params[:id])
-    if subscription.update(subscription_params)
+    if subscription.update!(subscription_params)
       render json: SubscriptionSerializer.new(subscription), status: :ok
     else
       render json: { error: "unable to cancel subscription" }, status: :bad_request
@@ -20,5 +22,11 @@ class Api::V1::SubscriptionsController < ApplicationController
   private
   def subscription_params
     params.permit(:customer_id, :tea_id, :title, :price, :status, :frequency)
+  end
+
+  def valid_status
+    if params[:status] != "active" && params[:status] != "canceled"
+      render json: { error: "invalid status" }, status: :bad_request
+    end
   end
 end
