9b3e94adaf6b1ab7e4a7d8360be61980961b3c41
Sam Smith <samlsmith424@gmail.com>
2022-06-01 22:49:43 -0700

    Feat: adjust factory decimal setting

--- spec/factories/subscriptions.rb
+++ spec/factories/subscriptions.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     customer
     tea
     title { "My #{Faker::Tea.variety} Subscription" }
-    price { 5.75 }
+    price { Faker::Number.decimal(r_digits: 2) }
     status { [0, 1].sample }
     frequency { [0, 1, 2].sample }
   end
