3836f09ae4dd76e89f6223f8368052b284c620d1
Sam Smith <samlsmith424@gmail.com>
2022-06-02 10:52:45 -0700

    Feat: remove index route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :customers, only: [:index, :create] do
+      resources :customers, only: [:create] do
         resources :subscriptions, only: [:index, :create, :update]
       end
       resources :teas, only: [:create]
