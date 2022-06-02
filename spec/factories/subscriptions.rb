90e3cc7b89511342190fedfb8407cc0846d728b4
Sam Smith <samlsmith424@gmail.com>
2022-06-01 18:31:29 -0700

    Refactor: fix typo

--- spec/factories/subscriptions.rb
+++ spec/factories/subscriptions.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :subscription do
     customer
     tea
-    title { "My #{Faker::Tea.varitety} Subscription" }
+    title { "My #{Faker::Tea.variety} Subscription" }
     price { 5.75 }
     status { [0, 1].sample }
     frequency { [0, 1, 2].sample }
